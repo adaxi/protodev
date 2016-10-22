2c85860c5d537ac16ce59a9f2ee7aef565d10410
adaxi <dev@adaxisoft.be>
2016-10-22 22:09:54 +0200

    Add default public domain license

--- script.sh
+++ script.sh
@@ -446,7 +446,7 @@ EOF
 		"vcs_url": "https://github.com/${TRAVIS_REPO_SLUG}.git",
 		"github_use_tag_release_notes": false,
 		"github_release_notes_file": "RELEASE.txt",
-		"licenses": [],
+		"licenses": [ "Public Domain" ],
 		"labels": [],
 		"public_download_numbers": false,
 		"public_stats": false,
